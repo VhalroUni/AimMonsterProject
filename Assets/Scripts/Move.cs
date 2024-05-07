af1467a6a34f3ed0ae4b6aa176d244c165ce5370
CarlaLoVi <clopezvi@edu.tecnocampus.cat>
2024-05-07 17:04:17 +0200

    Adds player animation

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,14 +8,14 @@ public class Move : MonoBehaviour
     public string VerticalAxisName = "Vertical";
     //Velocidad en público para que en cualquier momento podamos jugar con ella.
     public float speed = 5f;
+    public Animator animator;
 
     void Update()
     {
         //Esto nos ayuda a que la velocidad, en diagonal, no se duplique.
         Vector3 movement = new Vector3(Input.GetAxis(HorizontalAxisName), 0.0f, Input.GetAxis(VerticalAxisName)).normalized;
-
         transform.Translate(movement * speed * Time.deltaTime); 
-
+        animator.SetFloat("Blend", movement.magnitude != 0f ? 1f : 0f);
     }
 
 }
