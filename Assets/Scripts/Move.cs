cacc4e6276699d616aeabff705f92e69cb31e4d1
CarlaLoVi <clopezvi@edu.tecnocampus.cat>
2024-05-08 15:41:24 +0200

    Update Move

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -16,6 +16,7 @@ public class Move : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis(HorizontalAxisName), 0.0f, Input.GetAxis(VerticalAxisName)).normalized;
         transform.Translate(movement * speed * Time.deltaTime); 
         animator.SetFloat("Blend", movement.magnitude != 0f ? 1f : 0f);
+        
     }
 
 }
