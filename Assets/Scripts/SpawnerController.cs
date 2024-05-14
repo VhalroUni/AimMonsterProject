33c6776ba1de46a9389e7b1cf8f626ba6aa0c91b
VhalroUni <165813090+VhalroUni@users.noreply.github.com>
2024-05-14 17:12:40 +0200

    SpawnEnemigo FINALIZADO

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -2,55 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SpawnerController : MonoBehaviour
 {
-    public EnemySpawner spawnArea1;
-    public EnemySpawner spawnArea2;
-    public EnemySpawner spawnArea3;
-    public EnemySpawner spawnArea4;
-
-    private EnemySpawner[] allSpawnAreas;
+    private float nextspawntime;
+    public float InitialSpawnTime;
+    public float TimeToSpawn;
+    public float RandomTimeOffset;
+    public GameObject EnemyPrefab;
+    public EnemySpawner[] allSpawnAreas;
+    
     private EnemySpawner currentActiveArea;
 
     void Start()
     {
-        // Almacena todas las áreas de spawneo en un arreglo
-        allSpawnAreas = new EnemySpawner[] { spawnArea1, spawnArea2, spawnArea3, spawnArea4 };
-
-        // Desactiva todas las áreas de spawneo al inicio
-        DeactivateAllSpawnAreas();
-
         // Activa una de las áreas de spawneo de forma aleatoria
-        ActivateRandomSpawnArea();
+        // SpawnEnemyAtRandomSpawner();
+        nextspawntime = Time.time + InitialSpawnTime;
+
     }
 
     void Update()
     {
-        // Si la zona de spawneo actual ya no está activa, activa otra de forma aleatoria
-        if (currentActiveArea != null && !currentActiveArea.enabled)
-        {
-            ActivateRandomSpawnArea();
-        }
-    }
 
-    void DeactivateAllSpawnAreas()
-    {
-        foreach (var area in allSpawnAreas)
+        //verifica si es tiempo de spawnear un enemigo
+        if (Time.time >= nextspawntime)
         {
-            area.enabled = false;
+            SpawnEnemyAtRandomSpawner();
+            // calcula el tiempo para el próximo spawn sumando la frecuencia de spawn
+            nextspawntime = Time.time + (TimeToSpawn + Random.Range(-RandomTimeOffset,RandomTimeOffset));
         }
-    }
 
-    void ActivateRandomSpawnArea()
+    }
+    
+    void SpawnEnemyAtRandomSpawner()
     {
         // Selecciona una de las áreas de spawneo al azar
         int randomIndex = Random.Range(0, allSpawnAreas.Length);
         currentActiveArea = allSpawnAreas[randomIndex];
 
-        // Activa el área seleccionada y desactiva las demás
-        foreach (var area in allSpawnAreas)
-        {
-            area.enabled = (area == currentActiveArea);
-        }
+        currentActiveArea.SpawnEnemy(EnemyPrefab);    
     }
 }
