43138ae55d6a3734531f889d39de4295b41cfcaf
ErikSalguero <erodriguezsal@edu.tecnocampus.cat>
2024-05-17 15:52:00 +0200

    Proves respecte l'atac

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,10 +9,12 @@ public class EnemyController : MonoBehaviour
     public Transform Objective;
     public NavMeshAgent IA;
     public int damage;
-
+    public float delay = 6.0f;
+    public float lastAttack;
     void Start()
     {
         IA.SetDestination(Objective.position);
+        lastAttack = Time.time;
     }
     void Update()
     {
@@ -24,20 +26,29 @@ public class EnemyController : MonoBehaviour
         enemyHealth.TakeDamage(1);
         if (enemyHealth.health <= 0)
         {
-           Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.gameObject.CompareTag("Player"))
+        if (Time.time - lastAttack < delay)
+        {
             return;
-
+        }
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
         Health OtherHealth = collision.gameObject.GetComponent<Health>();
 
         if (OtherHealth != null)
         {
             OtherHealth.TakeDamage(damage);
             Debug.Log("Daño");
+            lastAttack = Time.time;
+            Debug.Log("Tiempo desde el último ataque: " + (Time.time - delay ));
+
         }
+
     }
 }
