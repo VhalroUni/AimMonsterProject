e48bd3b5bcc95d2ff766003ef849d367cf30a276
VhalroUni <165813090+VhalroUni@users.noreply.github.com>
2024-06-18 22:51:33 +0200

    Last prefabs

--- Assets/Scripts/Player/ClickHandler.cs
+++ Assets/Scripts/Player/ClickHandler.cs
@@ -5,13 +5,20 @@ using UnityEngine.EventSystems;
 
 public class ClickHandler : MonoBehaviour
 {
+
+    public GameObject particlesPrefab; // Asigna el prefab desde el Inspector
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Detecta si se hace clic con el botón izquierdo del mouse
         {
             if (!IsPointerOverUIObject())
             {
+                Debug.Log("No se hizo clic en un UI");
                 HandleClick();
+            }
+            else
+            {
+                Debug.Log("Se hizo clic en un UI");
             }
         }
     }
@@ -22,7 +29,16 @@ public class ClickHandler : MonoBehaviour
         eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
-        return results.Count > 0;
+
+        foreach (var result in results)
+        {
+            Debug.Log("Hit UI element: " + result.gameObject.name);
+            if (result.gameObject.CompareTag("UI"))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void HandleClick()
@@ -32,15 +48,24 @@ public class ClickHandler : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
+            Debug.Log("Raycast hit: " + hit.transform.name);
             if (hit.transform.gameObject.GetComponent<EnemyController>() == null)
             {
                 // No se hizo clic en un enemigo
                 SoundManager.instance.PlayEarthSound();
+
+                // Instancia el prefab de partículas
+                Instantiate(particlesPrefab, hit.point, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Clic en enemigo");
             }
         }
         else
         {
             // No se hizo clic en ningún objeto
+            Debug.Log("Raycast no hit");
             SoundManager.instance.PlayEarthSound();
         }
     }
